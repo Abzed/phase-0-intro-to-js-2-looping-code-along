bd4a7b4897df99ce6af4542ddb9fa0782e534b55
Abzed <ibraabzed@gmail.com>
2022-04-10 11:11:03 +0300

    commit tree 635feeed2663e35de066a5649f9c5fe7161f0af9

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+let myArr = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(arr){
+    let newArr = []
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push("Thank you, " + arr[i] + ", for the wonderful surprise gift!")       
+    }
+    return newArr; 
+}
+console.log(writeCards(myArr));
+
+
+function countDown(num){
+    for (let i = 0; i <= num; i++) {
+        console.log(i);
+        
+    }
+}
+countDown(10)
+
+
+
